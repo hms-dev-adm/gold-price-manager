0120a7a11c950c1d29c644a8a7b91d10a622d6cb
Heniinmay <realhayley05@gmail.com>
2025-07-10 17:33:15 +0900

    feat : mallId & auth code 받기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import React, {useState, useEffect }from 'react';
+import styled from 'styled-components';
+import {CAFE24_CONFIG} from './utils/constants'
+
 import './App.css';
 
 function App() {
+  const [mallId, setMallId] = useState('');
+  const [tokenUrl, setTokenUrl] = useState('');
+  const [authCode, setAuthCode] = useState('');
+  const [isConfigured, setIsConfigured] = useState('');
+  
+  //mall id & 인증코드 확인
+  useEffect(()=>{
+    const url = new URL(window.location.href);
+    const urlParams = url.searchParams;
+
+    //mall id
+    const mallIdParam = urlParams.get('mall_id');
+    const codeParam = urlParams.get('code');
+    const stateParam = urlParams.get('state');
+    
+    if(mallIdParam){
+      setMallId(mallIdParam);
+      generateTokenUrl(mallIdParam); //아래
+    }
+
+    //토큰 URL 생성
+    const generateTokenUrl = (targetMallId) =>{
+      setAuthCode(codeParam);
+    }
+  })
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
